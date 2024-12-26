a43ed1fc875015f9191541e4ff870966d3e0c653
hamoebgebey <hamosource@gmail.com>
2024-12-26 08:45:51 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -96,7 +96,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MISH0009/DNS",
+    "https://github.com/hamoebgebey/DNS",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
