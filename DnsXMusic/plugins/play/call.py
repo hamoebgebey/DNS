daeba2f229e52a5541a2fbd2db08afbfc9f93668
hamoebgebey <hamosource@gmail.com>
2024-12-27 03:37:41 +0200

    Update call.py

--- DnsXMusic/plugins/play/call.py
+++ DnsXMusic/plugins/play/call.py
@@ -5,26 +5,25 @@ from DnsXMusic import app
 
 @app.on_message(filters.voice_chat_started)
 async def stcall(client: Client, message: Message): 
-    start_text = "🎉 الكول اتفتح 🙈"
-    await message.reply_text(start_text)
+      Startt = "الكول اتفتح 🙈"
+      await message.reply_text(Startt)
 
 @app.on_message(filters.voice_chat_ended)
 async def encall(client: Client, message: Message): 
-    end_text = "👋 قفلت الكول ليه، ربنا يسمحك 😔😭"
-    await message.reply_text(end_text)
+      Enddd = "قفلت الكول ليه ربنا يسمحك 😔😭"
+      await message.reply_text(Enddd)
 
 @app.on_message(filters.voice_chat_members_invited)
 async def zoharyy(client: Client, message: Message): 
-    inviter = message.from_user.mention
-    invited_users = message.voice_chat_members_invited.users
-
-    if invited_users:
-        user_mentions = " ".join([f"[{user.first_name}](tg://user?id={user.id})" for user in invited_users])
-        text = f"-{inviter} قام بدعوة: {user_mentions}"
-    else:
-        text = f"-{inviter} قام بدعوة أعضاء إلى الكول."
-
-    try:
-        await message.reply(text)
-    except Exception as e:
-        print(f"Error replying to message: {e}")
+           text = f"-قام {message.from_user.mention}\n - بدعوة : "
+           x = 0
+           for user in message.voice_chat_members_invited.users:
+             try:
+               text += f"[{user.first_name}](tg://user?id={user.id}) "
+               x += 1
+             except Exception:
+               pass
+           try:
+             await message.reply(f"{text} ")
+           except:
+             pass
