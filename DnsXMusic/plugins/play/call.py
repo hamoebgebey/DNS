639937fe32faf1012ba2604e4514a8458a8345c6
hamoebgebey <hamosource@gmail.com>
2024-12-27 03:34:39 +0200

    Update call.py

--- DnsXMusic/plugins/play/call.py
+++ DnsXMusic/plugins/play/call.py
@@ -1,33 +1,30 @@
 import asyncio
 from pyrogram import Client, filters
-from strings import get_command
-import command
-from pyrogram.types import Message, VoiceChatStarted, VoiceChatEnded
-from DnsXMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from DnsXMusic.core.call import Anon
-from DnsXMusic.utils.database import get_assistant
+from pyrogram.types import Message
+from DnsXMusic import app 
 
 @app.on_message(filters.voice_chat_started)
 async def stcall(client: Client, message: Message): 
-      Startt = "الكول اتفتح 🙈"
-      await message.reply_text(Startt)
+    start_text = "🎉 الكول اتفتح 🙈"
+    await message.reply_text(start_text)
 
 @app.on_message(filters.voice_chat_ended)
 async def encall(client: Client, message: Message): 
-      Enddd = "قفلت الكول ليه ربنا يسمحك 😔😭"
-      await message.reply_text(Enddd)
+    end_text = "👋 قفلت الكول ليه، ربنا يسمحك 😔😭"
+    await message.reply_text(end_text)
 
 @app.on_message(filters.voice_chat_members_invited)
 async def zoharyy(client: Client, message: Message): 
-           text = f"-قام {message.from_user.mention}\n - بدعوة : "
-           x = 0
-           for user in message.voice_chat_members_invited.users:
-             try:
-               text += f"[{user.first_name}](tg://user?id={user.id}) "
-               x += 1
-             except Exception:
-               pass
-           try:
-             await message.reply(f"{text} ")
-           except:
-             pass
+    inviter = message.from_user.mention
+    invited_users = message.voice_chat_members_invited.users
+
+    if invited_users:
+        user_mentions = " ".join([f"[{user.first_name}](tg://user?id={user.id})" for user in invited_users])
+        text = f"-{inviter} قام بدعوة: {user_mentions}"
+    else:
+        text = f"-{inviter} قام بدعوة أعضاء إلى الكول."
+
+    try:
+        await message.reply(text)
+    except Exception as e:
+        print(f"Error replying to message: {e}")
